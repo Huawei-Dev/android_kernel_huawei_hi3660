d0cbd57f8142f307ec8e9a5815487934281f073d
Christian Brauner <christian@brauner.io>
2019-04-17 22:50:25 +0200

    BACKPORT: signal: support CLONE_PIDFD with pidfd_send_signal
    Let pidfd_send_signal() use pidfds retrieved via CLONE_PIDFD.  With this
    patch pidfd_send_signal() becomes independent of procfs.  This fullfils
    the request made when we merged the pidfd_send_signal() patchset.  The
    pidfd_send_signal() syscall is now always available allowing for it to
    be used by users without procfs mounted or even users without procfs
    support compiled into the kernel.
    
    Signed-off-by: Christian Brauner <christian@brauner.io>
    Co-developed-by: Jann Horn <jannh@google.com>
    Signed-off-by: Jann Horn <jannh@google.com>
    Acked-by: Oleg Nesterov <oleg@redhat.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: "Eric W. Biederman" <ebiederm@xmission.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: David Howells <dhowells@redhat.com>
    Cc: "Michael Kerrisk (man-pages)" <mtk.manpages@gmail.com>
    Cc: Andy Lutomirsky <luto@kernel.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Aleksa Sarai <cyphar@cyphar.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    
    (cherry picked from commit 2151ad1b067275730de1b38c7257478cae47d29e)
    
    Conflicts:
            kernel/sys_ni.c
    
    (1. Replaced COND_SYSCALL with cond_syscall.)
    
    Bug: 135608568
    Test: test program using syscall(__NR_sys_pidfd_open,..) and poll()
    Change-Id: I621fe6547397e0e68c560d7da60ef7715deb290c
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -2989,7 +2989,6 @@ SYSCALL_DEFINE2(kill, pid_t, pid, int, sig)
 	return kill_something_info(sig, &info, pid);
 }
 
-#ifdef CONFIG_PROC_FS
 /*
  * Verify that the signaler and signalee either are in the same pid namespace
  * or that the signaler's pid namespace is an ancestor of the signalee's pid
@@ -3009,6 +3008,14 @@ static bool access_pidfd_pidns(struct pid *pid)
 	}
 
 	return true;
+}
+
+static struct pid *pidfd_to_pid(const struct file *file)
+{
+	if (file->f_op == &pidfd_fops)
+		return file->private_data;
+
+	return tgid_pidfd_to_pid(file);
 }
 
 static int copy_siginfo_from_user_any(siginfo_t *kinfo, siginfo_t __user *info)
@@ -3062,7 +3069,7 @@ SYSCALL_DEFINE4(pidfd_send_signal, int, pidfd, int, sig,
 		return -EBADF;
 
 	/* Is this a pidfd? */
-	pid = tgid_pidfd_to_pid(f.file);
+	pid = pidfd_to_pid(f.file);
 	if (IS_ERR(pid)) {
 		ret = PTR_ERR(pid);
 		goto err;
@@ -3096,7 +3103,6 @@ err:
 	fdput(f);
 	return ret;
 }
-#endif /* CONFIG_PROC_FS */
 
 static int
 do_send_specific(pid_t tgid, pid_t pid, int sig, struct siginfo *info)
