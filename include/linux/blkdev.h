6ec760980af2eec002e133de5ec6b40299998c77
xNombre <kartapolska@gmail.com>
2021-05-25 23:49:06 +0200

    blkdev: Disable iostats and add_random by default
    It hurts performance.
    
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Signed-off-by: Sarthak Roy <sarthak.roy@s.amity.edu>
    Signed-off-by: sarthakroy2002 <sarthak.roy@s.amity.edu>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -869,15 +869,15 @@ struct request_queue {
 #define QUEUE_FLAG_QOS         27
 #endif
 
-#define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_IO_STAT) |		\
+#define QUEUE_FLAG_DEFAULT	((0 << QUEUE_FLAG_IO_STAT) |		\
 				 (1 << QUEUE_FLAG_STACKABLE)	|	\
 				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
-				 (1 << QUEUE_FLAG_ADD_RANDOM))
+				 (0 << QUEUE_FLAG_ADD_RANDOM))
 
-#define QUEUE_FLAG_MQ_DEFAULT	((1 << QUEUE_FLAG_IO_STAT) |		\
+#define QUEUE_FLAG_MQ_DEFAULT	((0 << QUEUE_FLAG_IO_STAT) |		\
 				 (1 << QUEUE_FLAG_STACKABLE)	|	\
 				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
-				 (1 << QUEUE_FLAG_POLL))
+				 (0 << QUEUE_FLAG_POLL))
 
 static inline void queue_lockdep_assert_held(struct request_queue *q)
 {
