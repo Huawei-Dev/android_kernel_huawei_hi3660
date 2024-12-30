c5699e16e5791fcfb9c8ab3c910babb1009a9bba
Huawei_Dev <surdu_petru@yahoo.com>
2024-12-30 17:13:45 +0100

    include: drop unused flag STRUCTLEAK_PLUGIN

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -27,11 +27,7 @@ extern void __chk_user_ptr(const volatile void __user *);
 extern void __chk_io_ptr(const volatile void __iomem *);
 # define ACCESS_PRIVATE(p, member) (*((typeof((p)->member) __force *) &(p)->member))
 #else /* __CHECKER__ */
-# ifdef STRUCTLEAK_PLUGIN
-#  define __user __attribute__((user))
-# else
-#  define __user
-# endif
+# define __user
 # define __kernel
 # define __safe
 # define __force
